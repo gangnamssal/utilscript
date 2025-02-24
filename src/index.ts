6412b13ffe61ad9f99d7e8e2f5d3fb1fe40816bb
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-02-24 17:36:48 +0900

    add: object/PartialReadonly util type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as A from './array';
 export * as C from './commonness';
+export * as O from './object';
 export * as P from './primitive';
 export * as S from './string';
---
+++ src/object/PartialReadonly/index.ts
@@ -0,0 +1,65 @@
+import { Alike, Expect } from '../../commonness';
+/**
+ * Make some properties readonly
+ *
+ * 일부 속성을 읽기 전용으로 만드는 타입
+ *
+ * @param T 객체
+ * @param K 읽기 전용으로 만들 속성
+ *
+ * @example
+ * type cases = [
+ *   Expect<
+ *     Alike<
+ *       PartialReadonly<{ name: string; age: number }, 'name'>,
+ *       { readonly name: string; age: number }
+ *     >
+ *   >,
+ * ]
+ */
+
+export type PartialReadonly<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P];
+} & Omit<T, K>;
+
+/**
+ * 테스트 코드
+ */
+
+// @ts-ignore
+
+if (process.env.NODE_ENV === 'development') {
+  interface Todo1 {
+    title: string;
+    description?: string;
+    completed: boolean;
+  }
+
+  interface Todo2 {
+    readonly title: string;
+    description?: string;
+    completed: boolean;
+  }
+
+  interface Expected {
+    readonly title: string;
+    readonly description?: string;
+    completed: boolean;
+  }
+
+  type cases = [
+    Expect<Alike<PartialReadonly<Todo1>, Readonly<Todo1>>>,
+    Expect<Alike<PartialReadonly<Todo1, 'title' | 'description'>, Expected>>,
+    Expect<Alike<PartialReadonly<Todo2, 'title' | 'description'>, Expected>>,
+    Expect<Alike<PartialReadonly<Todo2, 'description'>, Expected>>,
+    Expect<
+      Alike<
+        PartialReadonly<{ name: string; age: number }, 'name'>,
+        { readonly name: string; age: number }
+      >
+    >,
+  ];
+
+  // @ts-expect-error
+  type error = MyReadonly2<Todo1, 'title' | 'invalid'>;
+}
