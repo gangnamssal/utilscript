7b79198f0a0199bddca18c4c0c310d1f8dbde058
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-02-24 15:41:51 +0900

    add: primitive/AnyArray type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * as A from './array';
 export * as C from './commonness';
+export * as P from './primitive';
 // export * as S from './string';
---
+++ src/primitive/AnyArray/index.ts
@@ -0,0 +1,3 @@
+import { Tuple } from '../Tuple';
+
+export type AnyArray<T = unknown> = Array<T> | Tuple<T>;
