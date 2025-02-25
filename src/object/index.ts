03823b98ff7813c5e5fcc09b63ed0181d719875c
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-02-25 16:26:09 +0900

    add: object/Diff util type

--- src/object/index.ts
+++ src/object/index.ts
@@ -2,6 +2,7 @@ export * from './Alike';
 export * from './Append';
 export * from './DeepCopy';
 export * from './DeepReadonly';
+export * from './Diff';
 export * from './LookUp';
 export * from './Merge';
 export * from './PartialReadonly';
