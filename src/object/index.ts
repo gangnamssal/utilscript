7038627938b448a5c2c37576d964fda7c74a68c2
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-02-25 18:20:23 +0900

    add: object/PartialByKeys util type

--- src/object/index.ts
+++ src/object/index.ts
@@ -5,6 +5,7 @@ export * from './DeepReadonly';
 export * from './Diff';
 export * from './LookUp';
 export * from './Merge';
+export * from './PartialByKeys';
 export * from './PartialReadonly';
 export * from './PickByType';
 export * from './ReplaceKeys';
