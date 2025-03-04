35137e8474d9533583ba2a084755ba9c9005f8ef
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-03-04 22:54:19 +0900

    add: commonness/IsAny util type

--- src/commonness/index.ts
+++ src/commonness/index.ts
@@ -2,6 +2,7 @@ export * from './Equal';
 export * from './Expect';
 export * from './If';
 export * from './IsAlphabet';
+export * from './IsAny';
 export * from './IsNever';
 export * from './IsTuple';
 export * from './IsUnion';
