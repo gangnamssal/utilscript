8fde47e62f21e7addf430195026b95caa4004e06
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-03-09 11:30:14 +0900

    update: add test case in array/ToIndex

--- tests/array/ToIndex.test-d.ts
+++ tests/array/ToIndex.test-d.ts
@@ -2,6 +2,7 @@ import { ToIndex } from '../../src/array';
 import { Equal, Expect } from '../../src/commonness';
 
 type cases = [
+  Expect<Equal<ToIndex<[]>, []>>,
   Expect<Equal<ToIndex<[1, 2, 3]>, [0, 1, 2]>>,
   Expect<Equal<ToIndex<['a', 'b', 'c']>, [0, 1, 2]>>,
   Expect<Equal<ToIndex<[1, 'a', 'b', 'c']>, [0, 1, 2, 3]>>,
