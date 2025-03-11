735e661edf71223cc9ca92af53b6785fc5c6a2d2
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-03-12 00:16:24 +0900

    update: union string files jsDoc

--- src/union/Range/index.ts
+++ src/union/Range/index.ts
@@ -1,23 +1,28 @@
 import { Tuple } from '../../primitive';
 
 /**
+ *
  * Create a union of numbers in a given range
  *
  * 주어진 범위의 숫자 유니온을 생성
  *
- * @param L - Start of the range / 범위의 시작
- * @param H - End of the range / 범위의 끝
- * @param Result - Result to return / 반환할 결과
+ * @param L Start of the range / 범위의 시작
+ *
+ * @param H End of the range / 범위의 끝
+ *
+ * @param Result Result to return / 반환할 결과
  *
  * @example
  * type Result1 = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
  * type Result2 = 0 | 1 | 2;
- * 
+ *
  * type cases = [
  *   Expect<Equal<Range<2, 9>, Result1>>,
  *   Expect<Equal<Range<0, 2>, Result2>>,
  * ];
-
+ *
+ * @link https://www.utilscript.site/docs/usage-union/range
+ *
  */
 export type Range<
   L extends number,
--- src/union/ToIntersection/index.ts
+++ src/union/ToIntersection/index.ts
@@ -1,16 +1,21 @@
 /**
+ *
  * Convert union type to intersection type
  *
  * 유니온 타입을 교차 타입으로 변환
  *
- * @param U 유니온 타입
- * @returns 교차 타입
+ * @param U Union type / 유니온 타입
+ *
+ * @returns Intersection type / 교차 타입
  *
  * @example
  * type cases = [
  *   Expect<Equal<ToIntersection<'a' | 'b'>, 'a' & 'b'>>,
  *   Expect<Equal<ToIntersection<'a' | 'b' | 'c'>, 'a' & 'b' & 'c'>>,
  * ]
+ *
+ * @link https://www.utilscript.site/docs/usage-union/to-intersection
+ *
  */
 export type ToIntersection<U> = (U extends infer T ? (arg: T) => unknown : never) extends (
   arg: infer R,
