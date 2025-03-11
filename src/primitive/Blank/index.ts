437d6366169efc7ae7eb116d0629bbd0baa13d57
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-03-12 00:06:46 +0900

    update: modify primitive files jsDoc

--- src/primitive/Blank/index.ts
+++ src/primitive/Blank/index.ts
@@ -1,6 +1,10 @@
 /**
+ *
  * Type representing whitespace characters
  *
  * 공백 문자를 나타내는 타입
+ *
+ * @link https://www.utilscript.site/docs/usage-primitive/blank
+ *
  */
 export type Blank = ' ' | '\n' | '\t';
