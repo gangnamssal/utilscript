8db95e58d4e1863a850554e0d92478cb71a58550
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-03-28 10:45:14 +0900

    update: array/Chunk logic

--- tests/array/Chunk.test-d.ts
+++ tests/array/Chunk.test-d.ts
@@ -8,4 +8,14 @@ type cases = [
   Expect<Equal<Chunk<[1, 2, 3, 4], 2>, [[1, 2], [3, 4]]>>,
   Expect<Equal<Chunk<[1, 2, 3, 4], 5>, [[1, 2, 3, 4]]>>,
   Expect<Equal<Chunk<[1, true, 2, false], 2>, [[1, true], [2, false]]>>,
+  // 추가 테스트 케이스
+  Expect<Equal<Chunk<[1, 2, 3, 4, 5], 3>, [[1, 2, 3], [4, 5]]>>,
+  Expect<Equal<Chunk<[1, 2, 3], 0>, [1, 2, 3]>>,
+  Expect<Equal<Chunk<['a', 'b', 'c', 'd'], 2>, [['a', 'b'], ['c', 'd']]>>,
+  Expect<Equal<Chunk<[null, undefined, 0, false], 1>, [[null], [undefined], [0], [false]]>>,
+  Expect<Equal<Chunk<[1, 2, 3, 4, 5, 6], 4>, [[1, 2, 3, 4], [5, 6]]>>,
+  // readonly 엣지 케이스
+  Expect<Equal<Chunk<readonly [1, 2, 3], 2>, [[1, 2], [3]]>>,
+  Expect<Equal<Chunk<readonly [1, 2, 3, 4], 2>, [[1, 2], [3, 4]]>>,
+  Expect<Equal<Chunk<readonly [1, 2, 3, 4, 5], 3>, [[1, 2, 3], [4, 5]]>>,
 ];
