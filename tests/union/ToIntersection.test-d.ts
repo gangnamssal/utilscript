56265184e47d4d6233ed39164c77ba7fd026a6bc
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-04-01 10:29:46 +0900

    update: union/ToIntersection logic

--- tests/union/ToIntersection.test-d.ts
+++ tests/union/ToIntersection.test-d.ts
@@ -7,4 +7,13 @@ type cases = [
   Expect<
     Equal<ToIntersection<(() => 'foo') | ((i: 42) => true)>, (() => 'foo') & ((i: 42) => true)>
   >,
+  // 엣지 케이스
+  Expect<Equal<ToIntersection<never>, never>>,
+  Expect<Equal<ToIntersection<any>, any>>,
+  Expect<Equal<ToIntersection<unknown>, unknown>>,
+  Expect<Equal<ToIntersection<boolean>, true & false>>,
+  Expect<Equal<ToIntersection<null | undefined>, null & undefined>>,
+  Expect<Equal<ToIntersection<{} | []>, {} & []>>,
+  Expect<Equal<ToIntersection<string | number>, string & number>>,
+  Expect<Equal<ToIntersection<{ a: 1 } | { b: 2 }>, { a: 1 } & { b: 2 }>>,
 ];
