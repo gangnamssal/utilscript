dd4565419bfac1e9e8e409f2084b26163121fc8c
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-04-03 10:16:20 +0900

    update: object/GetToObject logic

--- tests/object/GetToObject.test-d.ts
+++ tests/object/GetToObject.test-d.ts
@@ -15,4 +15,14 @@ type cases = [
   Expect<Equal<GetToObject<{ a: { b: 1; c: 2 } }, 'a.c'>, { a: { c: 2 } }>>,
   Expect<Equal<GetToObject<{ a: 1; b: 2; c: 3 }, 'a'>, { a: 1 }>>,
   Expect<Equal<GetToObject<{ a: 1; b: 2; c: 3 }, 'c'>, { c: 3 }>>,
+
+  // 엣지 케이스
+  Expect<Equal<GetToObject<{}, 'key'>, never>>,
+  Expect<Equal<GetToObject<{ a: null }, 'a'>, { a: null }>>,
+  Expect<Equal<GetToObject<{ a: undefined }, 'a'>, { a: undefined }>>,
+  Expect<Equal<GetToObject<{ 'a.b.c': number }, 'a.b.c'>, { 'a.b.c': number }>>,
+  Expect<Equal<GetToObject<{ '': { empty: boolean } }, ''>, { '': { empty: boolean } }>>,
+  Expect<Equal<GetToObject<{ '': { empty: boolean } }, '.empty'>, { '': { empty: boolean } }>>,
+  Expect<Equal<GetToObject<{ foo: { bar: { baz: string } } }, 'foo.bar.baz.qux'>, never>>,
+  Expect<Equal<GetToObject<{ foo: { bar: { baz: string } } }, ''>, never>>,
 ];
