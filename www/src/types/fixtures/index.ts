72f8fdd207f31ff9ace6c0be4c0a335d97bc09df
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-04-09 16:30:55 +0900

    update: modified types/fixtures structure

--- www/src/types/fixtures/index.ts
+++ www/src/types/fixtures/index.ts
@@ -1,1 +1,9 @@
-export * from './page';
+import { P } from 'utilscript';
+
+export type BrandedCode = P.Branded<string, 'code'>;
+
+export type PositionMap<T extends string = string> = Record<T, { code: BrandedCode }>;
+
+export type BrandedActiveText = P.Branded<'Array' | 'Object' | 'String', 'activeText'>;
+
+export type ActiveTextMap<T extends string = string> = Record<T, BrandedActiveText>;
