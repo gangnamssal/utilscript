13d1ea853d986de34ef4712f138c2e8c13c18847
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-04-10 20:39:13 +0900

    update: array/Fill performance improvement

--- tests/array/Fill.test-d.ts
+++ tests/array/Fill.test-d.ts
@@ -20,4 +20,40 @@ type cases = [
   Expect<Equal<Fill<[1, 2, 3], 0, -1, 1>, [1, 2, 3]>>,
   Expect<Equal<Fill<readonly [1, 2, 3], 0>, [0, 0, 0]>>,
   Expect<Equal<Fill<[any], string>, [string]>>,
+  // 성능 테스트 케이스
+  Expect<Equal<Fill<[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0>, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]>>,
+  Expect<Equal<Fill<[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0, 3, 7>, [1, 2, 3, 0, 0, 0, 0, 8, 9, 10]>>,
+  Expect<
+    Equal<
+      Fill<[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 'x', 5, 10>,
+      [1, 2, 3, 4, 5, 'x', 'x', 'x', 'x', 'x', 11, 12, 13, 14, 15]
+    >
+  >,
+  Expect<
+    Equal<
+      Fill<[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], null, 0, 20>,
+      [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+      ]
+    >
+  >,
 ];
