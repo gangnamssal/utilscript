487c65737f6ec5802da80c31f761a9ec4b7b6368
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2025-04-10 15:57:40 +0900

    update: refectored files in string/

--- tests/string/Last.test-d.ts
+++ tests/string/Last.test-d.ts
@@ -1,7 +1,7 @@
 import { Equal, Expect } from '../../src/commonness';
 import { Last } from '../../src/string';
 
-export type cases = [
+type cases = [
   Expect<Equal<Last<'hello'>, 'o'>>,
   Expect<Equal<Last<'world'>, 'd'>>,
   Expect<Equal<Last<'typescript'>, 't'>>,
